a1688228725773e533df7404b795ba37977335c8
Radintha Dilshan <rasinthadilshan9@gmail.com>
2021-11-09 06:44:44 +0530

    Initial commit

---
+++ src/lk/ihse/hibernate/Appinitializer.java
@@ -0,0 +1,16 @@
+package lk.ihse.hibernate;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Appinitializer extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
